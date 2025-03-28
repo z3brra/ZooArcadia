70b9733c14b91d5ec588ced042a38ed16a29ed6f
z3brra <pas.tp2000@gmail.com>
2025-03-28 21:49:31 +0100

    Feature : Ajout des propriétés + fonctions pour DTO écriture et lecture

--- backend_arcadia/src/DTO/ActivityReadDTO.php
+++ backend_arcadia/src/DTO/ActivityReadDTO.php
@@ -8,7 +8,45 @@ use Symfony\Component\Serializer\Annotation\Groups;
 
 class ActivityReadDTO
 {
-    
+    #[Groups(['activity:read'])]
+    public string $uuid;
+
+    #[Groups(['activity:read'])]
+    public string $name;
+
+    #[Groups(['activity:read'])]
+    public ?string $description;
+
+    #[Groups(['activity:read'])]
+    public DateTimeImmutable $createdAt;
+
+    #[Groups(['activity:read'])]
+    public ?DateTimeImmutable $updatedAt;
+
+    public function __construct(
+        string $uuid,
+        string $name,
+        ?string $description = null,
+        DateTimeImmutable $createdAt,
+        ?DateTimeImmutable $updatedAt = null
+    ) {
+        $this->uuid = $uuid;
+        $this->name = $name;
+        $this->description = $description;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+    }
+
+    public static function fromEntity(Activity $activity): self
+    {
+        return new self(
+            $activity->getUuid(),
+            $activity->getName(),
+            $activity->getDescription(),
+            $activity->getCreatedAt(),
+            $activity->getUpdatedAt()
+        );
+    }
 }
 
 ?>
