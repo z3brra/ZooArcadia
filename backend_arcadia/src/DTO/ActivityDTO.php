5d6b0011fad7cf6ad09d3853a36872730cedabbb
z3brra <pas.tp2000@gmail.com>
2025-03-28 19:21:24 +0100

    Feature : Ajout des DTO pour l'entité Activity

---
+++ backend_arcadia/src/DTO/ActivityDTO.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\DTO;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ActivityDTO
+{
+
+}
+
+?>
+
---
+++ backend_arcadia/src/DTO/ActivityReadDTO.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\DTO;
+
+use App\Entity\Activity;
+use DateTimeImmutable;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+class ActivityReadDTO
+{
+    
+}
+
+?>
