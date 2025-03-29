fcd68244491bb9ad8afdb904154be9145a263c9a
z3brra <pas.tp2000@gmail.com>
2025-03-29 14:11:16 +0100

    Feature : Ajout endpoint update pour Activity

--- backend_arcadia/src/Controller/ActivityController.php
+++ backend_arcadia/src/Controller/ActivityController.php
@@ -12,7 +12,6 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 use App\Exception\ValidationException;
-use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpKernel\Exception\{BadRequestHttpException, NotFoundHttpException};
 
 #[Route('/api/activity', name: 'app_api_activity_')]
@@ -37,7 +36,7 @@ final class ActivityController extends AbstractController
                     'json'
                 );
             } catch (\Exception $e) {
-                throw new BadRequestException("Invalid JSON format");
+                throw new BadRequestHttpException("Invalid JSON format");
             }
 
             $activityReadDTO = $this->activityService->createActivity($activityCreateDTO);
@@ -77,7 +76,7 @@ final class ActivityController extends AbstractController
         }
     }
 
-    #[Route('{uuid}', name: 'show', methods: 'GET')]
+    #[Route('/{uuid}', name: 'show', methods: 'GET')]
     public function show(
         string $uuid
     ): JsonResponse {
@@ -101,6 +100,65 @@ final class ActivityController extends AbstractController
             return new JsonResponse(
                 data: ['error' => $e->getMessage()],
                 status: JsonResponse::HTTP_NOT_FOUND
+            );
+        } catch (\Exception $e) {
+            return new JsonResponse(
+                data: ['error' => "An internal server error as occured"],
+                status: JsonResponse::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    #[Route('/{uuid}', name: 'update', methods: 'PUT')]
+    public function update(
+        string $uuid,
+        Request $request
+    ): JsonResponse {
+        try {
+            try {
+                $activityUpdateDTO = $this->serializer->deserialize(
+                    data: $request->getContent(),
+                    type: ActivityDTO::class,
+                    format: 'json'
+                );
+            } catch (\Exception $e) {
+                throw new BadRequestHttpException("Invalid JSON format");
+            }
+
+            $activityReadDTO = $this->activityService->updateActivity($uuid, $activityUpdateDTO);
+
+            $responseData = $this->serializer->serialize(
+                data: $activityReadDTO,
+                format: 'json',
+                context: ['groups' => ['activity:read']]
+            );
+            $location = $this->urlGenerator->generate(
+                name: 'app_api_activity_show',
+                parameters: ['uuid' => $uuid],
+                referenceType: UrlGeneratorInterface::ABSOLUTE_URL
+            );
+
+            return new JsonResponse(
+                data: $responseData,
+                status: JsonResponse::HTTP_OK,
+                headers: ['Location' => $location],
+                json: true
+            );
+
+        } catch (NotFoundHttpException $e) {
+            return new JsonResponse(
+                data: ['error' => $e->getMessage()],
+                status: JsonResponse::HTTP_NOT_FOUND
+            );
+        } catch (BadRequestHttpException $e) {
+            return new JsonResponse(
+                data: ['error' => $e->getMessage()],
+                status: JsonResponse::HTTP_BAD_REQUEST
+            );
+        } catch (ValidationException $e) {
+            return new JsonResponse(
+                data: json_decode($e->getMessage(), true),
+                status: JsonResponse::HTTP_UNPROCESSABLE_ENTITY
             );
         } catch (\Exception $e) {
             return new JsonResponse(
