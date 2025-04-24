60fdf80fdfa88ce45c583a0b1d69c91562d1cd1a
z3brra <pas.tp2000@gmail.com>
2025-04-24 20:41:44 +0200

    Feature : Front - Correction erreur compilation vite.config.ts

--- frontend_arcadia/vite.config.ts
+++ frontend_arcadia/vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'url'
 
 // https://vite.dev/config/
-// config optimisée avec ChatGPT
+// config optimisée / corrigée avec ChatGPT
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@models": path.resolve(__dirname, 'src/models'),
-      "@assets": path.resolve(__dirname, 'src/assets'),
+      "@models": fileURLToPath(new URL('./src/models', import.meta.url)),
+      "@assets": fileURLToPath(new URL('./src/assets', import.meta.url)),
     }
   }
 })
