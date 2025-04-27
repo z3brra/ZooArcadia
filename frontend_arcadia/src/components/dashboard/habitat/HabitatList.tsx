581a47fe243c4358a20d43925866ef205dd4cd2e
z3brra <pas.tp2000@gmail.com>
2025-04-27 15:27:55 +0200

    Feature : Front - Ajout d'un key pour Habitat Item

--- frontend_arcadia/src/components/dashboard/habitat/HabitatList.tsx
+++ frontend_arcadia/src/components/dashboard/habitat/HabitatList.tsx
@@ -12,6 +12,7 @@ export function HabitatsList({ items }: HabitatListProps) {
         <DashboardSection className="habitats-list">
             {items.map(habitat => (
                 <HabitatItem
+                    key={habitat.uuid}
                     uuid={habitat.uuid}
                     imageUrl={habitat.pictures && habitat.pictures.length > 0 ? habitat.pictures[0].path : placeholderPicture}
                     name={habitat.name}
